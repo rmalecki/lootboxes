b4381bc51d4cd42e6ead87d9b97e365ee91f0f46
Rouven Malecki <rouven@robotriotgames.com>
2021-05-18 16:25:38 +0200

    Initial "divine intervention" logic

--- lib/lootboxes.ex
+++ lib/lootboxes.ex
@@ -26,6 +26,10 @@ defmodule Lootboxes do
     end)
   end
 
+  def picks_without_rarity(history, r) do
+    history |> Enum.take_while(&(&1 != r)) |> Enum.count()
+  end
+
   defp pick_rarity(history, probabilities) do
     x = :rand.uniform()
 
@@ -33,15 +37,17 @@ defmodule Lootboxes do
     adjusted_probabilities =
       probabilities
       |> Enum.zip(0..9)
-      |> Enum.map(fn {p, n} ->
-        expected_phase = 1 / min(1, p)
+      |> Enum.map(fn {p, r} ->
+        expected_phase = 1 / p - 1
         # historic picks without any of that rarity
-        historic_phase = history |> Enum.take_while(&(&1 != n)) |> Enum.count()
-
-        p + max(0, 1.0 - p) * historic_phase / expected_phase
+        historic_phase = picks_without_rarity(history, r)
+        if historic_phase > expected_phase, do: 1.0, else: p
+        min(1, p + max(0, 1.0 - p) *
+          :math.pow(max(0, historic_phase - expected_phase) / expected_phase, 5))
       end)
 
-    adjusted_probabilities |> inspect() |> IO.puts()
+    adjusted_probabilities = adjusted_probabilities
+      |> Enum.reverse() |> prioritize() |> Enum.reverse()
 
     r =
       Enum.reduce_while(probabilities, {0, 0}, fn p, {n, p_total} ->
@@ -54,5 +60,15 @@ defmodule Lootboxes do
   defp normalize(p) do
     total = Enum.sum(p)
     Enum.map(p, fn x -> x / total end)
+  end
+
+  def prioritize(probabilities, weight \\ 1)
+
+  def prioritize([_x], weight) do
+    [weight]
+  end
+
+  def prioritize([x | rest], weight) do
+    [x * weight | prioritize(rest, weight - x * weight)]
   end
 end
--- test/lootboxes_test.exs
+++ test/lootboxes_test.exs
@@ -2,111 +2,29 @@ defmodule LootboxesTest do
   use ExUnit.Case
   doctest Lootboxes
 
-  test "pick 100 random rarities" do
-    {pack, history} =
+  test "pick random rarities" do
+    {pack, _history} =
       Lootboxes.create_rarities(
-        [80, 20, 5, 1],
-        [
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          2,
-          1,
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          2,
-          1,
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          2,
-          1,
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          2,
-          1,
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          2,
-          1,
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          2,
-          1,
-          0,
-          0,
-          0,
-          0,
-          1,
-          0,
-          0,
-          0,
-          0,
-          3
-        ],
-        5
+        [89, 9, 2],
+        [],
+        1000
       )
 
-    pack |> inspect() |> IO.puts()
+    #0..2 |> Enum.map(fn r ->
+    #  Enum.count(history, &(&1 == r)) end)
+    #  |> inspect() |> IO.puts()
+
     assert Enum.all?(pack, fn x -> x >= 0 and x < 4 end)
+  end
+
+  test "history" do
+    assert [0, 0, 0, 2, 3, 1]
+           |> Lootboxes.picks_without_rarity(2) == 3
+  end
+
+  test "prioritize" do
+    assert [0.90, 0.8, 0.15]
+           |> Lootboxes.prioritize()
+           |> Enum.sum() == 1.0
   end
 end
